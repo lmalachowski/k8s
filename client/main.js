b9be93324e0997abdf684dbe344fdb86b95f2b07
Adam Marczyk <adam.marczyk90@gmail.com>
2019-12-06 11:08:15 +0100

    add endpoints

--- client/main.js
+++ client/main.js
@@ -2,30 +2,36 @@ var input = document.querySelector("input[type = 'text']");
 var ul = document.querySelector("ul");
 var spans = document.getElementsByTagName("span");
 
+(function loadTodo(){
+  axios.get('http://localhost:5000')
+})();
+
 function deleteTodo(){
     for(let span of spans){
       span.addEventListener ("click",function (){
-          //DELETE from API
+        axios.delete('http://localhost:5000/todos', {
+          todo: span.parentElement.lastChild.nodeValue.trimLeft(),
+        })
         span.parentElement.remove();
       });
     }
-  }
-
-function loadTodo(){
-    //GET from API
   }
 
 input.addEventListener("keypress",function(keyPressed){
     if(keyPressed.which === 13){
       var li = document.createElement("li");
       var spanElement = document.createElement("span");
-          
+
       var newTodo = this.value;
       this.value = " " ;
-          
+
       spanElement.append('usun');
       ul.appendChild(li).append(spanElement,newTodo);
-        //POST to API
-      }
+
+      axios.post('http://localhost:5000/todos', {
+        todo: newTodo,
+      })
+
+    }
     deleteTodo();
   });
